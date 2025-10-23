92bc2ac862e2e5a35c92cc4a8d85e72c33f095ce
Your Name <you@example.com>
2025-10-22 23:06:06 -0400

    10/22

---
+++ user/src/components/card/index.tsx
@@ -0,0 +1,15 @@
+import type { CardProps } from "../../types/card";
+
+
+const Card: React.FC<CardProps> = ({title, view, like}) => {
+
+
+    return(
+        <div className="card">
+            Card
+        </div>
+    )
+
+}
+
+export default Card;
--- user/src/types/card.ts
+++ user/src/types/card.ts
@@ -1,4 +1,7 @@
 export interface CardProps{
 
-    
+    title: string,
+    view: number,
+    like: number
+
 }
